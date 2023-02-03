d6555d251beca9e21b7a995421fdc9abc41d99d0
Vrishin Shah <vrishinshah02@gmail.com>
2023-02-04 02:18:10 +0530

    Init

---
+++ dms/informational/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def info(request):
+    return render(request, 'info.html')
+
+def donate(request):
+    return render(request, 'donate.html')
