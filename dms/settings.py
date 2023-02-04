c41ce5093a42847c64045a5bc7e8729a7ab64d66
Vrishin Shah <58455626+PicantaData@users.noreply.github.com>
2023-02-05 04:55:06 +0530

    Update settings.py

--- dms/settings.py
+++ dms/settings.py
@@ -118,8 +118,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
